75eab893ecfd3e17b1211dcb6d6185f0c154b47d
BogdanB0705 <71458999+BogdanB0705@users.noreply.github.com>
2021-06-03 15:21:43 -0400

    Delete BossDB directory

--- BossDB/Security/BossWeb.sql
+++
@@ -1,2 +0,0 @@
-﻿CREATE USER [BossWeb] FOR LOGIN [BossWeb];
-
--- BossDB/dbo/Tables/Customer_Group_Link.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Customer_Group_Link] (
-    [Cust_Seq_ID]    INT NULL,
-    [Cust_Group_Seq] INT NULL,
-    CONSTRAINT [FK_Customer_Group_Link_Customer] FOREIGN KEY ([Cust_Seq_ID]) REFERENCES [dbo].[Customer] ([Cust_Seq_ID]),
-    CONSTRAINT [FK_Customer_Group_Link_Customer_Group] FOREIGN KEY ([Cust_Group_Seq]) REFERENCES [dbo].[Customer_Group] ([Cust_Group_Seq])
-);
-
